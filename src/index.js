20a7e37cdc7ade98aee10505ef765bc71c69839a
lyin <ling95613@163.com>
2018-03-14 10:50:33 +0800

    简单实现爬虫

--- src/index.js
+++ src/index.js
@@ -1,20 +1,45 @@
 const http = require("http");
+const cheerio = require('cheerio');
 
-const url = 'http://blog.liaolunling.top';
+const url = 'http://blog.liaolunling.top/page/';
 
-http.get(url,(res)=>{
-  let data = "";
 
- // A chunk of data has been recieved.
-  res.on('data',(chunk)=>{
-    data += chunk;
+
+let i = 1;
+while(i<100){
+  fetchPage(url,i);
+  i++;
+}
+
+function fetchPage(url, page) {
+  console.log('-------------------',url+page)
+  http.get(url+page, (res) => {
+    let data = "";
+
+    // A chunk of data has been recieved.
+    res.on('data', (chunk) => {
+      data += chunk;
+    })
+
+
+    res.on('end', () => {
+      // console.log(data);
+      analyzeHtml(data);
+    })
+
+  }).on('error', (err) => {
+    console.log('Error:' + err.message);
   })
+}
 
 
-  res.on('end',()=>{
-    console.log(data);
-  })
+function analyzeHtml(html) {
+  const $ = cheerio.load(html);
+  const $postTitle = $('.post_title');  //article title
+  console.log($postTitle.length);
+  for (let i = 0; i < $postTitle.length; i++) {
+    const $postTitleItem = $($postTitle[i]);
+    console.log(i + 1, $postTitleItem.text(), $postTitleItem.find('a').attr('href'))
+  }
 
-}).on('error',(err)=>{
-  console.log('Error:err.message');
-})
+}
